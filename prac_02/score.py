bd9e94db267105d226bef4c2a3b397befe579fe0
lbw138286 <yuzhe.fu@my.jcu.edu.au>
2025-02-03 20:17:05 +0800

    print score

---
+++ prac_02/score.py
@@ -0,0 +1,32 @@
+import random
+
+def main():
+    score = get_score()
+    result = determine_result(score)
+    print(f"Result: {result}")
+
+    random_score = random.randint(0, 100)
+
+    random_result = determine_result(random_score)
+    print(f"Random score ({random_score}): {random_result}")
+
+def determine_result(score):
+    if 90 <= score <= 100:
+        return "Excellent"
+    elif 50 < score < 90:
+        return "Passable"
+    elif 0 <= score <= 50:
+        return "Bad"
+    else:
+        return "Invalid score"
+
+def get_score():
+    while True:
+        score_input = input("Enter score: ")
+
+        if score_input.replace('.', '', 1).isdigit() and score_input.count('.') < 2:  # To allow float input
+            return float(score_input)
+        else:
+            print("Invalid input. Please enter a numeric value.")
+
+main()
