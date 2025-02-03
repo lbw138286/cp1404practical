e55837da80aca38bdac6c8e3069672aa1e2e5df0
lbw138286 <yuzhe.fu@my.jcu.edu.au>
2025-02-03 15:37:04 +0800

    print password

---
+++ prac_02/password_stars.py
@@ -0,0 +1,12 @@
+MIN_LENGTH = 8
+
+def main():
+    password = input("Enter your password: ")
+
+    while len(password) < MIN_LENGTH:
+        print(f"Password must be at least {MIN_LENGTH} characters long.")
+        password = input("Enter your password: ")
+
+    print('*' * len(password))
+
+main()
